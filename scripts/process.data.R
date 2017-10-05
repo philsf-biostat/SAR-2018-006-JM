c7b18294c6e1f526c586e2616f647b1693e536fe
Felipe Figueiredo <philsf79@gmail.com>
2017-10-05 13:45:20 -0300

    no need to manually define the contrasts

--- scripts/process.data.R
+++ scripts/process.data.R
@@ -4,10 +4,5 @@ process.data <- function(dataframe) {
   DT <- data.table(dataframe)
   DT <- transform(DT, Sample = factor(Sample))
   levels(DT$Sample) <- c("bulk mono", "vacina formulada", "vacina liofilizada")
-  DT$form <- 0
-  DT$liof <- 0
-  DT[Sample == "vacina formulada"]$form <- 1
-  DT[Sample == "vacina liofilizada"]$liof <- 1
-  DT <- transform(DT, form = factor(form), liof = factor(liof))
   DT
 }
