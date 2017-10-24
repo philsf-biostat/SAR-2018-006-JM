a87b952fb92fd727fb0cd8aa3d89d83b6fd683e8
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-10-24 17:03:05 -0200

    prototype improvements: legend, transparency, jitter

--- scripts/kinetics-prototype.R
+++ scripts/kinetics-prototype.R
@@ -5,10 +5,24 @@ sar01 <- data.table(sar01)
 sar01b <- sar01[,.(Titulacao = mean(Titulacao), qPCR = mean(qPCR)), by = .(Coleta)]
 
 library(ggplot2)
+# mean dots
 ggplot(sar01b, aes(x = Coleta)) +
   geom_point(aes(y = Titulacao, shape = "Titulacao")) +
   geom_line(aes(y = Titulacao)) +
   geom_point(aes(y = qPCR, shape = "qPCR")) +
   geom_line(aes(y = qPCR)) +
-  theme(legend.title = element_text("Technique"))
+  scale_x_continuous(breaks = sar01$Coleta) +
+  scale_shape(name = "Technique") +
+  theme(legend.position = "bottom")
   
+# individual dots
+ggplot(sar01, aes(x = Coleta)) +
+  geom_jitter(aes(y = Titulacao, shape = "Titulacao"), alpha = .4, width = .1) +
+  geom_line(data = sar01b, aes(y = Titulacao)) +
+  geom_jitter(aes(y = qPCR, shape = "qPCR"), alpha = .4, width = .1) +
+  geom_line(data = sar01b, aes(y = qPCR)) +
+  scale_x_continuous(breaks = sar01$Coleta) +
+  scale_shape(name = "Technique") +
+  scale_color_discrete(name = "Experiment") +
+  theme(legend.position = "bottom")
+
