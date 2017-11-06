79168241a413417529be6d3b1c91b16252ef9853
Felipe Figueiredo <philsf79@gmail.com>
2017-11-06 15:49:35 -0200

    simplify mc.dem creation (with the SUPREME POWERRRR of data.table \m/)

--- scripts/mc.R
+++ scripts/mc.R
@@ -8,15 +8,10 @@ kinetics[Titulacao>0, .(R = cor(Titulacao, qPCR), R2=cor(Titulacao, qPCR)^2), by
 
 # data visualization ------------------------------------------------------
 
-mc.cax.01 <- with(kin.cax.01[Titulacao>0], Deming(Titulacao, qPCR))
-mc.cax.001 <- with(kin.cax.001[Titulacao>0], Deming(Titulacao, qPCR))
-mc.sar.01 <- with(kin.sar.01[Titulacao>0], Deming(Titulacao, qPCR))
-mc.sar.001 <- with(kin.sar.001[Titulacao>0], Deming(Titulacao, qPCR))
-mc.dem <- data.frame(rbind(mc.cax.01, mc.sar.01, mc.cax.001, mc.sar.001))
-mc.dem$Virus <- rep(c("Mumps", "Measles"), 2)
-mc.dem$MOI <- c("MOI 0.01", "MOI 0.01", "MOI 0.001", "MOI 0.001")
-mc.dem$Virus <- factor(mc.dem$Virus)
-mc.dem$MOI <- factor(mc.dem$MOI, levels = c("MOI 0.01", "MOI 0.001"))
+mc.dem <- (kinetics[Titulacao>0, 
+                     .( Intercept = Deming(Titulacao, qPCR)[1],
+                        Slope = Deming(Titulacao, qPCR)[2] ),
+                     by = .(MOI, Virus)])
 
 ggplot(kinetics[Titulacao>0], aes(Titulacao, qPCR, col = factor(Coleta))) +
   scale_color_discrete(name = "Time (h)") +
