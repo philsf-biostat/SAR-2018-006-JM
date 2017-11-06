7315b5042f3cf8e132108d6e5e133231581bc66d
Felipe Figueiredo <philsf79@gmail.com>
2017-11-06 15:20:59 -0200

    new data.frame from Deming parameters, for facets plot

--- scripts/mc.R
+++ scripts/mc.R
@@ -3,11 +3,19 @@ library(MethComp)
 library(ggplot2)
 
 mc.cax.01 <- with(kin.cax.01, Deming(Titulacao, qPCR))
+mc.cax.001 <- with(kin.cax.001, Deming(Titulacao, qPCR))
+mc.sar.01 <- with(kin.sar.01, Deming(Titulacao, qPCR))
+mc.sar.001 <- with(kin.sar.001, Deming(Titulacao, qPCR))
+mc.dem <- data.frame(rbind(mc.cax.01, mc.sar.01, mc.cax.001, mc.sar.001))
+mc.dem$Virus <- rep(c("Mumps", "Measles"), 2)
+mc.dem$MOI <- c("MOI 0.01", "MOI 0.01", "MOI 0.001", "MOI 0.001")
+mc.dem$Virus <- factor(mc.dem$Virus)
+mc.dem$MOI <- factor(mc.dem$MOI, levels = c("MOI 0.01", "MOI 0.001"))
 
 ggplot(kinetics[Titulacao>0], aes(Titulacao, qPCR, col = factor(Coleta))) +
   scale_color_discrete(name = "Time (h)") +
   theme(legend.position = "bottom") +
-  # geom_abline(intercept = mc.cax.01[1], slope = mc.cax.01[2]) +
+  geom_abline(aes(intercept = Intercept, slope = Slope), mc.dem) +
   facet_grid(Virus ~ MOI) +
   # geom_smooth(method = "Deming") +
   xlab("PFU/ml (log10)") + ylab("Copies/PCR (log10)") +
