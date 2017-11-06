4b5514c50df9e10d73fe4d842c43ecd919a129ad
Felipe Figueiredo <philsf79@gmail.com>
2017-11-06 14:50:11 -0200

    fix: remove Deming. Standalone ability and save figure

--- scripts/mc.R
+++ scripts/mc.R
@@ -1,10 +1,13 @@
+source("scripts/input.R")
 library(MethComp)
+library(ggplot2)
 
 mc.cax.01 <- with(kin.cax.01, Deming(Titulacao, qPCR))
 
 ggplot(kinetics[Titulacao>0], aes(Titulacao, qPCR)) +
   # geom_abline(intercept = mc.cax.01[1], slope = mc.cax.01[2]) +
   facet_grid(Virus ~ MOI) +
-  geom_smooth(method = "Deming") +
+  # geom_smooth(method = "Deming") +
   xlab("PFU/ml (log10)") + ylab("Copies/PCR (log10)") +
   geom_point()
+ggsave("figures/mc.png", width = 7, height = 7)
