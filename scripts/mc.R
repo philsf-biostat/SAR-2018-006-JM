a09f3931cf92abe8ab531031ae9057092ccc4ebc
Felipe Figueiredo <philsf79@gmail.com>
2017-11-06 14:59:37 -0200

    color by Time

--- scripts/mc.R
+++ scripts/mc.R
@@ -4,7 +4,9 @@ library(ggplot2)
 
 mc.cax.01 <- with(kin.cax.01, Deming(Titulacao, qPCR))
 
-ggplot(kinetics[Titulacao>0], aes(Titulacao, qPCR)) +
+ggplot(kinetics[Titulacao>0], aes(Titulacao, qPCR, col = factor(Coleta))) +
+  scale_color_discrete(name = "Time (h)") +
+  theme(legend.position = "bottom") +
   # geom_abline(intercept = mc.cax.01[1], slope = mc.cax.01[2]) +
   facet_grid(Virus ~ MOI) +
   # geom_smooth(method = "Deming") +
